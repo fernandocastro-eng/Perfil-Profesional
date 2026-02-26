d662067374d5b597668273e9e3df95d1cfb7ff7e
fernandocastro-eng <fernando.castro@estudiante.institutocomayagua.net>
2026-02-26 12:37:03 +0100

    Add files via upload

---
+++ misitiof-20260225T190829Z-1-001/misitiof/cproductos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href=".imgfares/favicon.png" type="image/png" sizes="16x16">
+    <title>inventario-Fares</title>
+</head>
+<body>
+    <tittle> inventario-Fares</tittle>
+
+    <form action="Guardar.php" method="post" enctype="multipart/form-data" autocomplete="off">
+      <input type="text" name="nproducto" id="nproducto"   patter="[A-Za-z] {3,}">
+    </form>
+    
+</body>
+</html>
